2e5fd82797d6f6f9383567beb699a98556618dbe
Zilox <89009170+Zilox135@users.noreply.github.com>
2022-05-28 20:20:03 +0100

    Updated,
    Added Military APC mod

---
+++ APC Mods/Mods/CustomParticleLoaderMultiExplosion/Scripts/MonoBehaviours/SubExplosionInitializer.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SubExplosionInitializer : MonoBehaviour
+{
+    public ExplosionData data;
+    public ItemValue value = null;
+    public int clrIdx = 0;
+    public EntityAlive entityAlive = null;
+    private bool explodeOnDeath = false;
+    private bool explodeOnCollision = true;
+    private ParticleSystem ps;
+    private List<ParticleCollisionEvent> list_events;
+    private World world;
+    //private BallisticParticleJob job;
+    private ParticleSystem.Particle[] particles;
+
+    void Awake()
+    {
+        ps = GetComponent<ParticleSystem>();
+        list_events = new List<ParticleCollisionEvent>();
+        world = GameManager.Instance.World;
+        var collision = ps.collision;
+        collision.collidesWith = Physics.AllLayers;
+    }
+
+    public void SetExplodeOnDeath(bool both)
+    {
+        explodeOnDeath = true;
+        explodeOnCollision = both;
+        particles = new ParticleSystem.Particle[ps.main.maxParticles];
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        if (!explodeOnCollision)
+            return;
+        int numCollisionEvents = ps.GetCollisionEvents(other, list_events);
+        //Log.Out("Particle collided! Count: " + numCollisionEvents.ToString());
+        int i = 0;
+        bool otherIsBorT = GameUtils.IsBlockOrTerrain(other.tag);
+        while (i < numCollisionEvents)
+        {
+            Vector3 pos = list_events[i].intersection + Origin.position;
+            Vector3 velocity = list_events[i].velocity;
+            //GameManager.Instance.ExplosionServer(0, pos, World.worldToBlockPos(pos), Quaternion.identity, data, entityid, 0, false, value);
+            //Log.Out("Explosion at: " + pos.ToString() + World.worldToBlockPos(pos).ToString());
+
+            Vector3 vec = otherIsBorT ? pos - velocity.normalized * 0.1f : pos;
+            Vector3i blockpos = World.worldToBlockPos(vec);
+            if (!world.GetBlock(blockpos).isair)
+                blockpos = Voxel.OneVoxelStep(blockpos, vec, -velocity.normalized, out vec, out BlockFace blockFace);
+
+            DoExplosionServer(vec, blockpos);
+            //Log.Out("Explosion at: " + vec.ToString() + blockpos.ToString());
+
+            /*
+            Ray ray = new Ray(pos - velocity, velocity.normalized);
+            float magnitude = velocity.magnitude + 1f;
+            bool flag = Voxel.Raycast(world, ray, magnitude, -538750981, 16 | 64, 0);
+            //Log.Out(pos.ToString() + velocity.ToString() + ray.ToString() + magnitude.ToString() + Voxel.voxelRayHitInfo.tag);
+            if (flag && (GameUtils.IsBlockOrTerrain(Voxel.voxelRayHitInfo.tag) || Voxel.voxelRayHitInfo.tag.StartsWith("E_")))
+            {
+                Vector3 vec = GameUtils.IsBlockOrTerrain(Voxel.voxelRayHitInfo.tag) ? Voxel.voxelRayHitInfo.hit.pos - velocity.normalized * 0.1f : pos;
+                Vector3i blockpos = World.worldToBlockPos(vec);
+                if (!world.GetBlock(blockpos).isair)
+                    blockpos = Voxel.OneVoxelStep(blockpos, vec, -velocity.normalized, out vec, out BlockFace blockFace);
+            
+                GameManager.Instance.ExplosionServer(Voxel.voxelRayHitInfo.hit.clrIdx, vec, blockpos, Quaternion.identity, data, entityid, 0, false, value);
+                Log.Out("Explosion at: " + vec.ToString() + blockpos.ToString());
+            }
+            */
+
+            i++;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (!explodeOnDeath)
+            return;
+
+        int count = ps.GetParticles(particles);
+        for(int i = 0; i < count; ++i)
+        {
+            if(particles[i].remainingLifetime <= Time.fixedDeltaTime)
+            {
+                Vector3 finalPos = particles[i].position + particles[i].totalVelocity * Time.fixedDeltaTime + Origin.position;
+                Vector3i blockPos = World.worldToBlockPos(finalPos);
+                if (!world.GetBlock(blockPos).isair)
+                    blockPos = Voxel.OneVoxelStep(blockPos, finalPos, -particles[i].totalVelocity.normalized, out finalPos, out BlockFace blockFace);
+                DoExplosionServer(finalPos, blockPos);
+                //Log.Out("Explode! remaining lifetime: " + particles[i].remainingLifetime);
+            }
+        }
+    }
+
+    private void DoExplosionServer(Vector3 worldPos, Vector3i blockPos)
+    {
+        if (entityAlive != null && value != null)
+        {
+            /*
+            if (otherIsBorT)
+            {
+                ChunkCluster chunkCluster = world.ChunkClusters[clrIdx];
+                if (chunkCluster != null)
+                {
+                    BlockValue blockValue = BlockValue.Air;
+                    blockValue = chunkCluster.GetBlock(blockpos);
+                    if (!blockValue.isair && blockValue.Block != null)
+                    {
+                        if (blockValue.ischild)
+                        {
+                            Vector3i pblockpos = blockValue.Block.multiBlockPos.GetParentPos(blockpos, blockValue);
+                            blockValue = chunkCluster.GetBlock(pblockpos);
+                        }
+                        if (!blockValue.Equals(BlockValue.Air))
+                        {
+                            entityAlive.MinEventContext.ItemValue = value;
+                            entityAlive.MinEventContext.BlockValue = blockValue;
+                            entityAlive.MinEventContext.Tags = blockValue.Block.Tags;
+                            entityAlive.FireEvent(MinEventTypes.onSelfDamagedBlock, false);
+                        }
+                    }
+                }
+            }
+            */
+            entityAlive.MinEventContext.Position = worldPos;
+            entityAlive.MinEventContext.ItemValue = value;
+            entityAlive.FireEvent(MinEventTypes.onProjectileImpact, false);
+
+            MinEventParams.CachedEventParam.Self = entityAlive;
+            MinEventParams.CachedEventParam.Position = worldPos;
+            MinEventParams.CachedEventParam.ItemValue = value;
+            value.ItemClass.FireEvent(MinEventTypes.onProjectileImpact, MinEventParams.CachedEventParam);
+        }
+        GameManager.Instance.ExplosionServer(clrIdx, worldPos, blockPos, Quaternion.identity, data, entityAlive != null ? entityAlive.entityId : -1, 0, false, value);
+    }
+
+    /*
+    private static bool DoVoxelCast(World world, Ray ray, float distance, float radius, int index, EntityAlive entityAlive, ItemValue itemValue)
+    {
+        bool hit = Voxel.Raycast(world, ray, distance, 16 | 64, radius);
+        if(hit && (GameUtils.IsBlockOrTerrain(Voxel.voxelRayHitInfo.tag) || Voxel.voxelRayHitInfo.tag.StartsWith("E_")))
+        {
+            if(entityAlive)
+            {
+                entityAlive.MinEventContext.Other = (ItemActionAttack.FindHitEntity(Voxel.voxelRayHitInfo) as EntityAlive);
+                entityAlive.MinEventContext.Position = Voxel.voxelRayHitInfo.hit.pos;
+                entityAlive.MinEventContext.ItemValue = itemValue;
+                entityAlive.FireEvent(MinEventTypes.onProjectileImpact, false);
+
+                if(itemValue != null)
+                {
+                    MinEventParams.CachedEventParam.Self = entityAlive;
+                    MinEventParams.CachedEventParam.Position = Voxel.voxelRayHitInfo.hit.pos;
+                    MinEventParams.CachedEventParam.ItemValue = itemValue;
+                    itemValue.ItemClass.FireEvent(MinEventTypes.onProjectileImpact, MinEventParams.CachedEventParam);
+                }
+            }
+            Vector3 vector2 = Voxel.voxelRayHitInfo.hit.pos - ray.direction * 0.1f;
+            Vector3i vector3i = World.worldToBlockPos(vector2);
+            if (!world.GetBlock(vector3i).isair)
+                vector3i = Voxel.OneVoxelStep(vector3i, vector2, -ray.direction.normalized, out vector2, out BlockFace blockFace);
+
+            if (CustomParticleEffectLoader.GetCustomParticleComponents(index, out CustomParticleComponents component))
+                world.gameManager.ExplosionServer(Voxel.voxelRayHitInfo.hit.clrIdx, vector2, vector3i, Quaternion.identity, component.BoundExplosionData, entityAlive ? entityAlive.entityId : -1, 0, false, itemValue);
+        }
+        return hit;
+    }
+
+    private struct BallisticParticleJob : IJobParticleSystem
+    {
+        int entityId;
+        int itemClassId;
+        int particleIndex;
+        public BallisticParticleJob(int entityId, int itemClassId, int particleIndex)
+        {
+            this.entityId = entityId;
+            this.itemClassId = itemClassId;
+            this.particleIndex = particleIndex;
+        }
+        void IJobParticleSystem.Execute(ParticleSystemJobData particles)
+        {
+            World world = GameManager.Instance.World;
+            if (world == null)
+                return;
+            EntityAlive entity = world.GetEntity(entityId) as EntityAlive;
+            ItemValue value = null;
+            if (itemClassId >= 0)
+                value = new ItemValue(itemClassId);
+            //Log.Out("particle job: itemclass " + itemClassId + " particle index " + particleIndex + " entity id " + entityId + " particle count: " + particles.count);
+            for (int i = 0; i < particles.count; ++i)
+            {
+                Vector3 pos = particles.positions[i];
+                Vector3 vel = particles.velocities[i];
+                Ray ray = new Ray(pos + Origin.position, vel.normalized);
+                if (SubExplosionInitializer.DoVoxelCast(world, ray, vel.magnitude * Time.fixedDeltaTime, 0, particleIndex, entity, value))
+                {
+                    var aliveTime = particles.aliveTimePercent;
+                    aliveTime[i] = 100;
+                    //Log.Out("Voxel cast hit!");
+                }
+            }
+        }
+    }
+    */
+}
+
+
---
+++ APC Mods/Mods/VehicleWeapon/Scripts/VehicleParts/VehicleWeaponBase.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleWeaponBase : VehicleWeaponPartBase
+{
+    protected bool hasOperator = false;
+    protected EntityPlayerLocal player = null;
+    protected string notReadySound = string.Empty;
+    protected string notOnTargetSound = string.Empty;
+    protected string activationSound = string.Empty;
+    protected string deactivationSound = string.Empty;
+    protected VehicleWeaponRotatorBase rotator = null;
+    protected int seat = 0;
+    protected int slot = int.MaxValue;
+    protected FiringJuncture timing;
+    protected bool pressed = false;
+    protected VehicleWeaponBase cycleNext = null;
+    protected float cycleInterval = 0f;
+    protected float cycleCooldown = 0f;
+    protected bool activated = true;
+
+    protected enum FiringJuncture
+    {
+        Anytime,
+        OnTarget,
+        FirstShot,
+        FirstShotOnTarget,
+        Cycle,
+        FromSlotKey,
+        FromSlotKeyOnTarget
+    }
+    public bool HasOperator { get => hasOperator; }
+    public bool Activated { get => activated; }
+    public VehicleWeaponRotatorBase Rotator { get => rotator; }
+    public int Seat { get => seat; }
+    public int Slot { get => slot; set => slot = value; }
+    public bool IsCurCycle { get; set; } = false;
+
+    public override void SetProperties(DynamicProperties _properties)
+    {
+        base.SetProperties(_properties);
+        _properties.ParseString("notReadySound", ref notReadySound);
+        _properties.ParseString("notOnTargetSound", ref notOnTargetSound);
+        _properties.ParseString("activationSound", ref activationSound);
+        _properties.ParseString("deactivationSound", ref deactivationSound);
+        string str = null;
+        _properties.ParseString("fireWhen", ref str);
+        if (!string.IsNullOrEmpty(str))
+            Enum.TryParse<FiringJuncture>(str, true, out timing);
+
+        _properties.ParseInt("seat", ref seat);
+        if (seat < 0)
+        {
+            Log.Error("seat can not be less than 0! setting to 0...");
+            seat = 0;
+        }
+        _properties.ParseInt("slot", ref slot);
+
+        player = GameManager.Instance.World.GetPrimaryPlayer();
+    }
+
+    public override void InitPrefabConnections()
+    {
+        base.InitPrefabConnections();
+
+        string rotatorName = null;
+        properties.ParseString("rotator", ref rotatorName);
+        if (!string.IsNullOrEmpty(rotatorName))
+            rotator = vehicle.FindPart(rotatorName) as VehicleWeaponRotatorBase;
+
+        if (rotator != null)
+            rotator.SetWeapon(this);
+    }
+
+    public virtual void SetupWeaponConnections(in List<VehicleWeaponBase> weapons)
+    {
+        if (timing != FiringJuncture.Cycle)
+            return;
+
+        int nextIndex = -1;
+        properties.ParseInt("cycleNextSlot", ref nextIndex);
+        if(nextIndex < 0 || nextIndex >= weapons.Count)
+        {
+            Log.Error("cycle index out of range!");
+            return;
+        }
+        cycleNext = weapons[nextIndex];
+        properties.ParseFloat("cycleInterval", ref cycleInterval);
+        if (nextIndex < slot)
+            cycleNext.IsCurCycle = true;
+    }
+
+    public virtual void SetCurCycle()
+    {
+        IsCurCycle = true;
+        cycleCooldown = cycleInterval;
+    }
+
+    public override void NoPauseUpdate(float _dt)
+    {
+        if (IsCurCycle && cycleCooldown > 0)
+            cycleCooldown -= _dt;
+
+        if (rotator != null)
+            rotator.NoPauseUpdate(_dt);
+    }
+
+    public override void NoGUIUpdate(float _dt)
+    {
+        if (rotator != null)
+            rotator.NoGUIUpdate(_dt);
+    }
+
+    public virtual void NetSyncUpdate(float horRot, float verRot)
+    {
+        if (rotator != null)
+            rotator.NetSyncUpdate(horRot, verRot);
+    }
+
+    public virtual void OnPlayerEnter()
+    {
+        hasOperator = true;
+
+        if (activated)
+            OnActivated();
+    }
+
+    public virtual void OnPlayerDetach()
+    {
+        hasOperator = false;
+        pressed = false;
+
+        OnDeactivated();
+    }
+
+    protected virtual void OnActivated()
+    {
+        if (rotator != null)
+            rotator.CreatePreview();
+    }
+
+    protected virtual void OnDeactivated()
+    {
+        if (rotator != null)
+            rotator.DestroyPreview();
+    }
+
+    protected void ToggleActivated()
+    {
+        activated = !activated;
+        if (activated)
+        {
+            OnActivated();
+            vehicle.entity.PlayOneShot(activationSound);
+        }
+        else
+        {
+            OnDeactivated();
+            vehicle.entity.PlayOneShot(deactivationSound);
+        }
+    }
+
+    public virtual void HandleUserInput(int userData)
+    {
+        var vw_input = PlayerActionsVehicleWeapon.Instance;
+        if (slot < vw_input.ActivateActions.Count && (vw_input.ActivateActions[slot].IsPressed || vw_input.ActivateActions[slot].WasReleased))
+        {
+            if (vw_input.HoldToggleActivated.IsPressed)
+            {
+                if(vw_input.ActivateActions[slot].WasPressed)
+                    ToggleActivated();
+            }
+            else
+            {
+                bool firstShot = (userData & 1) > 0;
+                DoFireLocal(ref firstShot, vw_input.ActivateActions[slot].WasReleased, true);
+            }
+        }
+    }
+
+    public void DoFireLocal(ref bool firstShot, bool isRelease, bool fromSlot = false)
+    {
+        if(DoFire(firstShot, isRelease, fromSlot))
+        {
+            firstShot = false;
+            Fired();
+        }
+    }
+
+    protected virtual bool DoFire(bool firstShot, bool isRelease, bool fromSlot)
+    {
+        if (!activated)
+            return false;
+
+        switch (timing)
+        {
+            case FiringJuncture.Anytime:
+                break;
+            case FiringJuncture.OnTarget:
+                if (rotator != null && !rotator.OnTarget)
+                {
+                    if(!pressed)
+                        vehicle.entity.PlayOneShot(notOnTargetSound);
+                    pressed = true;
+                    return false;
+                }
+                break;
+            case FiringJuncture.FirstShot:
+                if (!firstShot)
+                    return false;
+                break;
+            case FiringJuncture.FirstShotOnTarget:
+                if (!firstShot)
+                    return false;
+                else if (rotator != null && !rotator.OnTarget)
+                    goto notOnTarget;
+                break;
+            case FiringJuncture.Cycle:
+                if (cycleNext == null || !IsCurCycle || cycleCooldown > 0)
+                    return false;
+                break;
+            case FiringJuncture.FromSlotKey:
+                if (!fromSlot)
+                    return false;
+                break;
+            case FiringJuncture.FromSlotKeyOnTarget:
+                if (!fromSlot)
+                    return false;
+                else if (rotator != null && !rotator.OnTarget)
+                    goto notOnTarget;
+                break;
+        }
+        return true;
+    notOnTarget:
+        if (!pressed)
+            vehicle.entity.PlayOneShot(notOnTargetSound);
+        pressed = true;
+        return false;
+    }
+
+    protected virtual void Fired()
+    {
+        if (timing == FiringJuncture.Cycle)
+        {
+            IsCurCycle = false;
+            cycleNext.SetCurCycle();
+        }
+    }
+}
+
