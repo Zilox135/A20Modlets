4da99ca4327a9ebe6a16243f3cd75e73f8038979
Zilox <89009170+Zilox135@users.noreply.github.com>
2022-05-29 15:14:23 +0100

    Updated.

--- Mods/VehicleWeapon/Scripts/Input/PlayerActionVehicleWeaponData.cs
+++
@@ -1,6 +0,0 @@
-﻿public class PlayerActionVehicleWeaponData : PlayerActionData
-{
-    //public static readonly ActionTab TabVehicleWeapon = new ActionTab("inpTabVehicleWeapon", 16);
-    public static readonly ActionGroup GroupVehicleWeapon = new ActionGroup("inpGrpVehicleWeaponBasicName", null, 40, TabVehicle);
-}
-
